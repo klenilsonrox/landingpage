a0cd11f10fd3362385a075b62e2b0aed6f6865e9
klenilsonrox <klenilsoncoc@gmail.com>
2023-11-03 01:13:42 -0300

    att

--- src/config/conectDb.js
+++ src/config/conectDb.js
@@ -2,19 +2,18 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config()
 
-const port = process.env.DB_PORT
 const user = process.env.DB_USER
 const pass = process.env.DB_PASSWORD
 
 const mongo_Url= `mongodb+srv://${user}:${pass}@leads.bbiey0t.mongodb.net/`
-const options = {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}
+// const options = {
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// }
 
 export const conectDB = async ()=>{
     try{
-        await mongoose.connect(mongo_Url, options)
+        await mongoose.connect(mongo_Url)
         console.log("conectado ao banco mongodb com sucesso.")
     }catch(error){
         console.log("nao foi possivel conectar ao banco")
--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use("/api", routerUsers)
-
+app.use("/", (req,res)=>{
+    res.json({
+        status:"ok"
+    })
+})
 
 conectDB()
 
